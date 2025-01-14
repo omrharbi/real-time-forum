331a158cb88724e5c8aedd59c79323ea17a0d7a6
omrharbi <rharbu.omar@gmail.com>
2025-01-14 19:09:57 +0100

    done

--- backend/repo/likeRepo.go
+++ backend/repo/likeRepo.go
@@ -13,7 +13,7 @@ import (
 type likesRepository interface {
 	InserLike(ctx context.Context, user_id, card_id, is_liked int, UserLiked, Userdisliked bool) (m messages.Messages)
 	GetuserLiked(ctx context.Context, card_id int) []models.ResponseUserLikeds
-	GetLikes(ctx context.Context, post_id int) (int, int, int, int)
+	GetLikes(ctx context.Context, post_id int) (int, int, int)
 	DeletLike(ctx context.Context, user_id, card_id int)
 	LikeExists(ctx context.Context, user_id, card_id int) bool
 }
@@ -27,28 +27,22 @@ func NewLikesRepository(db *sql.DB) likesRepository {
 }
 
 // GetLikes implements likesRepository.
-func (l *likeRepositoryImpl) GetLikes(ctx context.Context, post_id int) (int, int, int, int) {
-	querylike := `SELECT  COALESCE(UserLiked,0), COALESCE(Userdisliked,0) , COALESCE(SUM(l.is_like), 0)  FROM post
-	 p, likes l WHERE p.card_id = l.card_id AND l.is_like = 1 AND p.id = ` + strconv.Itoa(post_id)
+func (l *likeRepositoryImpl) GetLikes(ctx context.Context, post_id int) (int, int, int) {
+	querylike := `SELECT   DISTINCT c.user_id , (SELECT count(*) FROM likes l WHERE ( l.card_id = c.id ) and l.is_like = 1) 
+	likes , 
+	(SELECT count(*) FROM likes l WHERE ( l.card_id = c.id ) and l.is_like = 0) dislikes FROM post  p
+      LEFT JOIN card  c ON p.card_id=c.id    JOIN  likes l WHERE p.card_id = l.card_id AND (l.is_like = 1 or l.is_like = 0 ) AND p.id = ? ` + strconv.Itoa(post_id)
 	like := 0
-	UserLiked := 0
-	UserdiLiked := 0
-	Userdisliked := 0
+	dislike := 0
+	userliked := 0
 
-	err := l.db.QueryRowContext(ctx, querylike).Scan(&UserLiked, &Userdisliked, &like)
+	err := l.db.QueryRowContext(ctx, querylike).Scan(&userliked, &like, &userliked)
 	if err != nil {
 		fmt.Println(err)
 		like = 0
 	}
-	querydislike := `SELECT COALESCE(UserLiked,0) ,COALESCE(Userdisliked,0) , COALESCE(SUM(l.is_like), 0) FROM 
-	post p, likes l WHERE p.card_id = l.card_id AND l.is_like = -1 AND p.id = ` + strconv.Itoa(post_id)
-	dislike := 0
 
-	err = l.db.QueryRowContext(ctx, querydislike).Scan(&UserdiLiked, &Userdisliked, &dislike)
-	if err != nil {
-		dislike = 0
-	}
-	return like, dislike * -1, UserLiked, Userdisliked
+	return like, dislike, userliked
 }
 
 // GetuserLiked implements likesRepository.
