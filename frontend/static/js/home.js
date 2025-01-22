aec0963bf5d5b8a52db4eb6e634bc9d5bb8556cb
yasine bahbib <yassine.bahbib@usmba.ac.ma>
2025-01-22 14:44:32 +0100

    good

--- frontend/static/js/home.js
+++ frontend/static/js/home.js
@@ -1,57 +1,31 @@
-let nav_items = document.querySelector("#home")//search
-let categories = document.querySelector("#categories")//search
-let profile = document.querySelector("#profile")//search
-let settings = document.querySelector("#settings")
+import { leftside } from "./component.js";
 
-import { checklogin } from "./checklogin.js";
+// import { checklogin } from "./checklogin.js";
 
-await checklogin()
- function navigate(){
+// await checklogin(id);
+function navigate(id) {
+  const div = document.querySelector(`#${id}`);
+  div.addEventListener("click", (e) => {
+    const currentUrl = window.location.pathname;
+    if (currentUrl !== `/${id}`) {
+      for (let idl of ids) {
+        document.getElementById(idl).classList.remove("active");
+      }
+      history.pushState(null, "", `/${id}`);
+      div.classList.add("active");
+    } else {
+      e.preventDefault();
 
-    nav_items.addEventListener("click", (e) => {
-        const currentUrl = window.location.pathname;
-        if (currentUrl !== "/home") {
-            location.href = "/home";
-        } else {
-            e.preventDefault()
-    
-            console.log("Already on the Home page, no need to reload.");
-        }
-    })
-    
-    profile.addEventListener("click", (e) => {
-        e.preventDefault()
-        const currentUrl = window.location.pathname;
-        if (currentUrl !== "/profile") {
-            location.href = "/profile";
-        } else {
-            console.log("Already on the profile page, no need to reload.");
-        }
-    })
-
-    categories.addEventListener("click", (e) => {
-        e.preventDefault()
-        const currentUrl = window.location.pathname;
-        if (currentUrl !== "/categories") {
-            location.href = "/categories";
-        } else {
-            console.log("Already on the profile page, no need to reload.");
-        }
-    })
-
-    settings.addEventListener("click", (e) => {
-        e.preventDefault()
-        const currentUrl = window.location.pathname;
-        if (currentUrl !== "/settings") {
-            location.href = "/settings";
-        } else {
-            console.log("Already on the profile page, no need to reload.");
-        }
-    })
-
+      console.log("Already on the Home page, no need to reload.");
+    }
+  });
 }
-navigate()
-export {
-    navigate
+
+const ids = ["home", "categories", "profile", "settings"];
+
+leftside();
+for (let idl of ids) {
+  navigate(idl);
 }
- 
+
+export { navigate };
